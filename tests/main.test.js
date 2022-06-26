92370fbf69f0e804105541a60c2b0989db739a57
Austen Stone <austenstone@github.com>
2022-06-26 11:48:29 -0400

    cleanup

--- tests/main.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globals_1 = require("@jest/globals");
-(0, globals_1.test)('Test 1', () => {
-    (0, globals_1.expect)(true).toBe(true);
-});
